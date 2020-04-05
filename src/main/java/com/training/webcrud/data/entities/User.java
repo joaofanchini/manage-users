e164f79cbadbd7cc8920878c0c5036da923ea218
João Fanchini <joaofanchini@gmail.com>
2020-04-05 00:04:16 -0300

    Adicionando listener JPA para gerar hash de senhas

--- src/main/java/com/training/webcrud/data/entities/User.java
+++ src/main/java/com/training/webcrud/data/entities/User.java
@@ -2,20 +2,30 @@ package com.training.webcrud.data.entities;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "TB_USER")
 public class User {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Id
     @Column(name = "USERNAME", length = 20)
     private String username;
     @Column(name = "PASSWORD", length = 100, nullable = false)
     private String password;
+
+    @PrePersist
+    @PreUpdate
+    private void preInsertOrUpdate(){
+        if(this.password != null)
+            this.password = passwordEncoder.encode(this.password);
+    }
 }
